42c226a0bfdc4da4d89691af7728deaa479d41fa
Sam Hill <s.hill@yorksj.ac.uk>
2026-04-23 13:29:46 +0100

    Remove version value

--- src/waveequation/__init__.py
+++ src/waveequation/__init__.py
@@ -1,6 +1,3 @@
 """Simple module for numerically solving the 2D wave equation"""
 
 from .PDESolver2d import WaveSolver  # noqa: F401
-from importlib.metadata import version
-
-__version__ = version("waveequation")
